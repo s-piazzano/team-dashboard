9447ce3423f740cf7a62cf6546bef49541925e29
Marco Terzolo <marco.terzolo.8080@gmail.com>
2020-12-19 12:19:02 +0100

    updated sidebar with narrow mode

--- src/components/lists/menu-items-list/menu-items-list.jsx
+++ src/components/lists/menu-items-list/menu-items-list.jsx
@@ -2,13 +2,27 @@ import React from "react"
 import { SvgIcon } from "../../buttons-indicators/svg-icon/svg-icon";
 import './menu-items-list.scss';
 
-export const MenuItemsList = ({items = []}) => {
+export const MenuItemsList = ({items = [], mode}) => {
+
+ const isExpanded = mode === 'expanded'
+ const [active, setActive] = React.useState(undefined)
+ 
   return (
-    <div className="items-container">
+    <div className={`items-container ${isExpanded ? '' : 'narrow'}`}>
       {items.map((item, i) => 
-        <button className="menu-item" key={i}>
-          <SvgIcon iconColor='#9a9b9c' iconName={item.icon}></SvgIcon><p className="item-label">{item.name}</p>
-        </button>
+        <a
+          href={`#${item.value}`}
+          className={`menu-item ${item.value === active ? 'clicked' : ''}`}
+          key={i}
+          onClick={() => setActive(item.value)}
+        >
+          <div className="item-icon">
+            <SvgIcon
+              iconColor={item.value === active ? '#a90926' : '#9a9b9c'} iconName={item.icon}>
+            </SvgIcon>
+          </div>
+          <p className="item-label">{item.name}</p>
+        </a>
       )}
     </div>
   )
--- src/components/sidebars/sidebar-menu/sidebar-menu.jsx
+++ src/components/sidebars/sidebar-menu/sidebar-menu.jsx
@@ -5,45 +5,52 @@ import 'smtp-webcomponents'
 import "./sidebar-menu.scss"
 import { MenuItemsList } from "../../lists/menu-items-list/menu-items-list"
 import { HeaderTitle } from "../../header/header-title/header-title"
+import { SvgIcon } from '../../buttons-indicators/svg-icon/svg-icon';
 
-export const SidebarMenu = () => {
+export const SidebarMenu = ({sidebarIsOpen, toggleSidebar}) => {
 
 		const logoUrl = 'https://picsum.photos/300/200';
-		const title = 'SMTProject';
+		const title = 'RainbowTech';
 		const subtitle = 'Where code happens';
 
 		const items = [
 			{
 				name: 'Chi siamo',
-        icon: 'icon-plus',
-        value: 'chi-siamo'
+        icon: 'icon-group',
+        value: 'members'
       },
       {
 				name: 'Portfolio',
-        icon: 'icon-hand-o-right',
+        icon: 'icon-flask',
         value: 'portfolio'
       },
       {
 				name: 'Utlimi articoli',
-        icon: 'icon-plus',
-        value: 'ultimi-articoli'
+        icon: 'icon-coffee',
+        value: 'blog'
       }
-		]
-
+    ]
 
     return (
-      <div className='sidebar-menu'>
-          <HeaderTitle
-            logoUrl={logoUrl}
-            title={title}
-            subtitle={subtitle}
+      <div className={`sidebar-menu ${sidebarIsOpen ? 'expanded' : 'narrow'}`}>
+        <div className={`sidebar-toggle ${sidebarIsOpen ? 'icon-left' : 'icon-right'}`}>
+          <SvgIcon
+            iconName="icon-arrow-circle-right"
+            iconClick={toggleSidebar}
           />
-          <MenuItemsList
-            items={items}
-            color="#f30e97"
-          />
-          <FooterSocial />
-
+        </div>
+        <HeaderTitle
+          logoUrl={logoUrl}
+          title={title}
+          subtitle={subtitle}
+          mode={sidebarIsOpen ? 'expanded' : 'narrow'}
+        />
+        <MenuItemsList
+          items={items}
+          mode={sidebarIsOpen ? 'expanded' : 'narrow'}
+          color="#f30e97"
+        />
+        <FooterSocial />
       </div>
     )
 }
